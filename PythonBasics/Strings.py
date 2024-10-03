4f0f731e6a837b7c78748ede2f5c7af4072f7bd5
MatchaNischala <NischalaMatcha23@outlook.com>
2024-10-03 21:47:55 +0530

    Commit4:DataTypes & Type keyword

---
+++ PythonBasics/Strings.py
@@ -0,0 +1,31 @@
+#Assigning strings to a variable
+
+name='cognizant'
+print(name)
+
+ #Multiline Strings
+para="""Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt, 
+ut labore et dolore magna aliqua"""
+print(para)
+
+
+ #Looping through a string and 'Len' keyword
+lang=['English','Telugu','Hindi']
+size=len(lang)
+for i in lang:
+    print("Languages:"+i)
+print("Length of the string is:")
+print(size)
+
+ #Checking for the text in the string
+my_name= "Matcha Nischala"
+val='Nischala'
+if val in my_name:
+    print("yes,it's present")
+else:
+    print("No, it's missing")
+
+
+
