2702c93077edc1e2eb21e18e08a6e223c3825f4e
MatchaNischala <NischalaMatcha23@outlook.com>
2024-10-05 15:00:53 +0530

    Updated the child class with gender property

--- PythonBasics/Inheritance.py
+++ PythonBasics/Inheritance.py
@@ -2,12 +2,12 @@
 # from socket import send_fds
 
 
-#We can inherit the methods and properties in 3 ways
+#We can inherit the methods and properties in 3 ways without mentioning any properties in child class
       # 1. By using 'Pass' keyword,it inherits the parent class properties or methods,
       #      when you do not want to add any other properties or methods to the class.
       # 2. By calling Parent's __init__() function
       # 3. By using 'Super' function,which will make the child class inherit all the methods and properties from its parent
-
+#We can inherit the methods of parents class and defining properties in child class init() function / over riding the parents init() function
 # Examples:
 
 
@@ -16,7 +16,7 @@ class Employees: # class creation
         self.emp_name=name
         self.emp_age=age
     def emp_info(self): # Function/Method definition in Parent class
-        print("\n In Parent Method")
+        print(" Method From Parent class")
         print(f" Employee details: "+self.emp_name,self.emp_age)
 
 employee=Employees("Liliya",6) # Creating object 'emp1'
@@ -38,3 +38,33 @@ class emp(Employees):
         Employees.__init__(self,name,age)
 emp2=emp("Nischala",33)
 emp2.emp_info()
+
+# 3. By using 'Super' function
+print("\n Example for Super function")
+class Emp(Employees):
+    def __init__(self,name,age):
+        super().__init__(name,age)
+emp3=emp("Angelina",33)
+emp3.emp_info()
+
+#
+class Employees: # class creation
+    def __init__(self,name,age): # Defining the properties
+        self.emp_name=name
+        self.emp_age=age
+    def emp_info(self): # Function/Method definition in Parent class
+        print(" Method From Parent class")
+        print(f" Employee details: "+self.emp_name,self.emp_age)
+
+
+class Employee(Employees): # Created'Employee' child class,which inherits from 'Employees' parent class
+    def __init__(self, name, age,gender):
+        super().__init__(name, age)
+        self.gender=gender
+emp3=Employee("Victor",33,"Male")
+print("Employee gender:"+emp3.gender)
+emp3.emp_info()
+
+
+
+
