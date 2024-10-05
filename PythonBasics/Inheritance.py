e3af4bfc8cc07c66993a3b4e37ed18a4cf52b09d
MatchaNischala <NischalaMatcha23@outlook.com>
2024-10-05 14:11:47 +0530

    This file contains below changes:  1. Implementing Inheritance by creating child class  2. Ways to inherit the properties and methods from Parent class

---
+++ PythonBasics/Inheritance.py
@@ -0,0 +1,40 @@
+# Inheritance: Allows us to inherit all the methods and properties from a Parent Class to Child class
+# from socket import send_fds
+
+
+#We can inherit the methods and properties in 3 ways
+      # 1. By using 'Pass' keyword,it inherits the parent class properties or methods,
+      #      when you do not want to add any other properties or methods to the class.
+      # 2. By calling Parent's __init__() function
+      # 3. By using 'Super' function,which will make the child class inherit all the methods and properties from its parent
+
+# Examples:
+
+
+class Employees: # class creation
+    def __init__(self,name,age): # Defining the properties
+        self.emp_name=name
+        self.emp_age=age
+    def emp_info(self): # Function/Method definition in Parent class
+        print("\n In Parent Method")
+        print(f" Employee details: "+self.emp_name,self.emp_age)
+
+employee=Employees("Liliya",6) # Creating object 'emp1'
+print(employee.emp_name,employee.emp_age)
+
+#Creating Child class and inheriting the Parent properties
+class emp(Employees):
+# 1. By using 'Pass' keyword:
+ pass
+print("Example for Pass keyword")
+print("\n In CHILD CLASS")
+emp1=emp("Meenu",6)
+emp1.emp_info()
+
+# 2. By calling Parent's __init__() function
+print("\n Example for Parent's init function")
+class emp(Employees):
+    def __init__(self,name,age):
+        Employees.__init__(self,name,age)
+emp2=emp("Nischala",33)
+emp2.emp_info()
